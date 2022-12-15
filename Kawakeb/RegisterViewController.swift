d52747eb1c44335e18703b559f156ab80cac002f
Raniaalageel <rania.kh.ala@gmail.com>
2022-12-15 15:08:33 +0300

    register

---
+++ Kawakeb/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  Kawakeb
+//
+//  Created by Rania Alageel on 21/05/1444 AH.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
