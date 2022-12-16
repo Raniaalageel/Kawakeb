f06f6f1d930710e1c9d4c72d811912730d5fe637
Raniaalageel <rania.kh.ala@gmail.com>
2022-12-17 01:04:39 +0300

    registration

---
+++ Kawakeb/HomeScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeScreenViewController.swift
+//  Kawakeb
+//
+//  Created by Rania Alageel on 22/05/1444 AH.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
--- Kawakeb/RegisterViewController.swift
+++ Kawakeb/RegisterViewController.swift
@@ -6,34 +6,63 @@
 //
 
 import UIKit
-import Firebase
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
 
 
 
-class RegisterViewController: UIViewController {
-    
-    
-    
-    
 
+class RegisterViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var bithdayTextField: UITextField!
+    
+    @IBOutlet weak var girl: UIButton!
+    @IBOutlet weak var boy: UIButton!
+    
+    @IBOutlet weak var selectedImage: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+
+    @IBOutlet weak var CreateAccountButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        self.emailTextField.delegate = self
+//        self.passwordTextField.delegate = self
+        
+        
     }
     
+    
+    @IBAction func CreateAccountPressed(_ sender: UIButton) {
+        
+//        guard let email = emailTextField.text else {return}
+//        guard let password = passwordTextField.text else {return}
+//            Auth.auth().createUser(withEmail: email, password: password) { authResult , error in
+//                if let e = error {
+//                    print(e.localizedDescription)
+//                   }else {
+//                     // Go to our home screen
+//                    self.performSegue(withIdentifier: "goToNext", sender: self)
+//                }
+//            }
+        }
+  
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    func Birthday() {
+//
+//    }
+    
 }
+
+
+
