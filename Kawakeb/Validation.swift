d2bb8b915420c24068721bf66c3083c76d7f5bda
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-12-17 18:51:48 +0300

    adult verification done  :) + login done :)

---
+++ Kawakeb/Validation.swift
@@ -0,0 +1,99 @@
+//
+//  Validation.swift
+//  Kawakeb
+//
+//  Created by Amani Aldahmash on 17/12/2022.
+//
+
+import UIKit
+
+class Validation: UIViewController {
+
+    @IBOutlet weak var equation: UILabel!
+    @IBOutlet weak var answer: UITextField!
+    @IBOutlet weak var submitB: UIButton!
+    
+    @IBOutlet weak var msg: UILabel!
+    
+    var actual: Int!
+    var ans: Int?
+    var  x1: Int!
+    var  x2: Int!
+    var  x: Character!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        msg.isHidden = true
+         actual = eq()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func eq() -> Int{
+        x1 = Int(exactly: arc4random_uniform(10) + 1)!
+        x2 =  Int(exactly: arc4random_uniform(10) + 1)!
+        let op = Int.random(in: 0..<4)
+        switch op {
+            case 0:
+            equation.text = "\(x1!) + \(x2!)"
+            return x1 + x2
+            case 1:
+            equation.text = "\(x1!) - \(x2!)"
+            return x1 - x2
+            case 2:
+            equation.text = "\(x1!) / \(x2!)"
+            return x1 / x2
+            case 3:
+            equation.text = "\(x1!) * \(x2!)"
+            return x1 * x2
+        default:
+            equation.text =  "  "
+            return 0
+        }
+   
+    }
+
+    @IBAction func pressed(_ sender: Any) {
+        let check = answer.text
+        let bl = (check?.isEmpty)!
+        
+        if(bl){
+            msg.isHidden = false
+            msg.text = "الرجاء ادخال رقم صحيح"
+        }
+        else{
+           let b2 = check?.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil
+            if b2{
+        ans = Int(answer.text!)!
+        if (actual==ans)
+        {
+            self.performSegue(withIdentifier: "checked", sender: self)
+        }
+        else
+        {
+            let alert = UIAlertController(title: "تنبيه", message: "اجابة خاطئة،  حاول مجددًا!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "حسنًا", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        
+        }}
+            else{
+                msg.isHidden = false
+                msg.text = "الرجاء ادخال رقم صحيح"
+            }
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
