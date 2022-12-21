7a4fab71792c336a7c2d76b028d31d34031c79e5
Raniaalageel <rania.kh.ala@gmail.com>
2022-12-21 21:46:34 +0300

    registration validation done

---
+++ RegValidation.swift
@@ -0,0 +1,117 @@
+//
+//  RegValidation.swift
+//  Kawakeb
+//
+//  Created by Rania Alageel on 27/05/1444 AH.
+//
+
+import UIKit
+
+class RegValidation: UIViewController {
+
+    
+    
+    @IBOutlet weak var equation: UILabel!
+    @IBOutlet weak var answer: UITextField!
+    @IBOutlet weak var submitB: UIButton!
+
+    @IBOutlet weak var msg: UILabel!
+    
+
+    
+    
+    let formatter: NumberFormatter = NumberFormatter()
+    
+    
+    var actual: Int!
+    var ans: Int?
+    var  x1: Int!
+    var  x2: Int!
+    var  x: Character!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        msg.isHidden = true
+         actual = eq()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func eq() -> Int{
+        x1 = Int(exactly: arc4random_uniform(10) + 1)!
+        x2 =  Int(exactly: arc4random_uniform(10) + 1)!
+        let op = Int.random(in: 0..<3)
+        switch op {
+            case 0:
+            equation.text = "\(x1!) + \(x2!)"
+            return x1 + x2
+            case 1:
+            equation.text = "\(x1!) - \(x2!)"
+            return x1 - x2
+//            case 2:
+//            equation.text = "\(x1!) / \(x2!)"
+//            return x1 / x2
+            case 2:
+            equation.text = "\(x1!) * \(x2!)"
+            return x1 * x2
+        default:
+            equation.text =  "  "
+            return 0
+        }
+   
+    }
+
+    @IBAction func pressed(_ sender: Any) {
+//        formatter.locale = NSLocale(localeIdentifier: "EN") as Locale?
+//        let final = formatter.number(from: answer.text!)
+//        var check = Int(final)
+//            print("\(check)")
+        let check = answer.text
+        
+        let bl = (check?.isEmpty)!
+
+        if(bl){
+            msg.isHidden = false
+            msg.text = "الرجاء ادخال رقم صحيح"
+        }
+        else{
+        let b2 = check?.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil
+            if b2{
+                formatter.locale = NSLocale(localeIdentifier: "EN") as Locale?
+                let final = formatter.number(from: check!)!
+                       var ans = Int(final)
+                           print("\(ans)")
+       // ans = Int(answer.text!)!
+        if (actual==ans)
+        {
+            self.performSegue(withIdentifier: "goToEmailPage", sender: self)
+        }
+        else
+        {
+            let alert = UIAlertController(title: "تنبيه", message: "اجابة خاطئة،  حاول مجددًا!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "حسنًا", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+
+        }}
+            else{
+                msg.isHidden = false
+                msg.text = "الرجاء ادخال رقم صحيح"
+            }
+        }
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
