3693e85dea49684f698fd1811e6e2d22c06afc43
sara81a <98774608+sara81a@users.noreply.github.com>
2022-12-31 00:27:38 +0300

    all function of move character is done and camea is open in correct way

--- Kawakeb/FirstGame.swift
+++ Kawakeb/FirstGame.swift
@@ -9,35 +9,74 @@
 import UIKit
 import AVFoundation
 import Vision
+import SpriteKit
+
 
 class FirstGame: UIViewController {
+    
+    var scene: FirstSKScene?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("nn")
+        
+        CameraOpen().viewDidLoad()
+        
+       // trytocall()
+        
+        
+        
+        //        if let view = self.view as? SKView, scene == nil {
+        //                    let scene = FirstSKScene(size: view.bounds.size)
+        //                    view.presentScene(scene)
+        //                    self.scene = scene
+        //                }
+    }
+    func trytocallSCene(){
+        //  CameraOpen().viewDidLoad()
+        print(view.frame)
+        let sceneView = SKView(frame: view.frame)
+        let scene = FirstSKScene()
+        scene.size = view.frame.size
+        //view.addSubview(sceneView)
+        sceneView.presentScene(scene)
+        self.view.addSubview(sceneView)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 7) {
+            sceneView.removeFromSuperview()
+        }
+        
+        
+        let v = UILabel(frame: .init(x: 100, y: 100, width: 100, height: 60))
+        v.text = "Hello"
+        self.view.addSubview(v)
+    }
+    
+    
+    @IBAction func gobutton(_ sender: UIButton) {
+        print("go is pressed")
+        
+        Global.shared.enabled = true //add all array
+        Global.shared.stopis = true   //stopcamer
+        CameraOpen().Stopsessyion()
+        
+        trytocallSCene()  //GO TOSKSCENE
+        
+        
+        Global.shared.allLablels = []
+        print("again open camera ")
+        Global.shared.stopis = false  //OPEN CAMERA
+        Global.shared.enabled = false //not appned array
+        CameraOpen().viewDidLoad()   //call class CameraOpen
 
-       
-       
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           print("nn")
-           
-   trytocall()
-       }
-       func trytocall(){
-           CameraOpen().viewDidLoad()
-       }
-       
-
-       @IBAction func gobutton(_ sender: UIButton) {
-           print("go is pressed")
-           Global.shared.enabled = true
-           Global.shared.stopis = true
-           
-         CameraOpen().Stopsessyion()
-   //        print("session.isRunning",session.isRunning )
-   //        if session.isRunning {
-   //                DispatchQueue.global().async {
-   //                print("STOP Session")
-   //                    self.session.stopRunning()
-   //                }
-   //            }
-      }
-
-   }
+        
+        
+        //        print("session.isRunning",session.isRunning )
+        //        if session.isRunning {
+        //                DispatchQueue.global().async {
+        //                print("STOP Session")
+        //                    self.session.stopRunning()
+        //                }
+        //            }
+    }
+    
+}
---
+++ Kawakeb/FirstSKScene.swift
@@ -0,0 +1,117 @@
+//
+//  FirstSKScene.swift
+//  Kawakeb
+//
+//  Created by Sara Alsaleh on 05/06/1444 AH.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+import CoreMotion
+
+
+class FirstSKScene: SKScene,SKPhysicsContactDelegate  {
+
+    var player:SKSpriteNode!
+
+    var xAcceration:CGFloat = 0
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        print("didMove", frame)
+        ///
+        
+        let background = SKSpriteNode(imageNamed: "background")
+            background.size = self.size
+            background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+            background.zPosition = 0
+            self.addChild(background)
+        ///
+        //
+//        let button = SKSpriteNode(imageNamed: "star11")
+//        button.name = "btn"
+//             button.size.height = 100
+//             button.size.width = 100
+//             button.position =  CGPoint(x: self.size.width/3, y: self.size.height/3)
+//             self.addChild(button)
+        
+        var buttongo: SKNode! = nil
+        buttongo = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 44))
+             //  buttongo.position = CGPoint(x:self.frame.midX, y:self.frame.midY);
+        buttongo.position = CGPoint(x: 120, y: 100);
+       
+//        buttongo(normalString: "انطلق", normalStringColor: UIColor.blueColor, size: CGSizeMake(200, 40), cornerRadius: 10.0, buttonFunc: gobutton() )
+               self.addChild(buttongo)
+        //
+        player = SKSpriteNode(imageNamed: "rocket1")  //shuttle rocket1.png
+       // player.position = CGPoint(x: 200, y: player.size.height + 100)
+        player.size = .init(width: 100, height: 100)
+        player.position = CGPoint(x: frame.width * 0.5, y: frame.height * 0.5)
+        self.addChild(player)
+        
+//        let action = SKAction.sequence([.move(by: .init(dx: -100, dy: 0), duration: 1), .rotate(byAngle: 10, duration: 1)]) action.timingMode = .easeOut
+        
+        if Global.shared.allLablels .isEmpty {
+            print("charcter not move")
+        }
+        else{
+            print("charcter  move")
+            var alliteration = 0
+            
+            while alliteration < Global.shared.allLablels.count  {
+                var ch = Character(":")
+     var result = Global.shared.allLablels[alliteration].split(separator: ch)
+                var resullabel = String(result[0])
+                
+                if(resullabel == "right"){
+                    print(" move right")
+                    let actionplayer = SKAction.move(by: .init(dx: 300, dy: 0), duration: 2)
+                    
+                    player.run(actionplayer)
+                }
+                else if(resullabel == "left"){
+                    print(" move left")
+                    let actionplayer = SKAction.move(by: .init(dx: -250, dy: 0), duration: 2)
+                    
+                    player.run(actionplayer)
+                    
+                }else if(resullabel == "stop"){
+                    let actionplayer = SKAction.rotate(byAngle: 10, duration: 2)
+                    
+                    player.run(actionplayer)
+                    
+                }
+               else  if(resullabel == "start"){
+                    
+                }
+                else if(resullabel == "up"){
+                    print(" move up")
+                    let actionplayer = SKAction.move(by: .init(dx: 0, dy: 100), duration: 2)
+                    
+                    player.run(actionplayer)
+                    
+                }else if(resullabel == "down"){
+                    print(" move down")
+                    let actionplayer = SKAction.move(by: .init(dx: 0, dy: -100), duration: 2)
+                    
+                    player.run(actionplayer)
+                    
+                }
+                alliteration += 1
+            }
+           
+        }
+             //player.zPosition = -1
+//        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    
+    func gobutton(){
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
