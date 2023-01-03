3a8dc91f12a812f279a76540b1db4e178e5e7e40
sara81a <98774608+sara81a@users.noreply.github.com>
2023-01-03 12:16:00 +0300

    commit all picture and alert

---
+++ AlertFail.swift
@@ -0,0 +1,21 @@
+//
+//  AlertFail.swift
+//  Kawakeb
+//
+//  Created by Sara Alsaleh on 10/06/1444 AH.
+//
+
+import UIKit
+
+class AlertFail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ AlertScs.swift
@@ -0,0 +1,21 @@
+//
+//  AlertScs.swift
+//  Kawakeb
+//
+//  Created by Sara Alsaleh on 10/06/1444 AH.
+//
+
+import UIKit
+
+class AlertScs: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
