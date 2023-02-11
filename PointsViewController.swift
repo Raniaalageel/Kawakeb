9e74c98bdb399e00a7fd23c962fd303c7b046a1d
sara81a <98774608+sara81a@users.noreply.github.com>
2023-02-12 01:03:03 +0300

    all points

---
+++ PointsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PointsViewController.swift
+//  Kawakeb
+//
+//  Created by Sara Alsaleh on 21/07/1444 AH.
+//
+
+import UIKit
+
+class PointsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
